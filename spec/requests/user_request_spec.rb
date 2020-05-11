ae4896d0c9974d3da71cdbb572b5164bbd9eca9c
Pedro Hoppe <pedrohbh@github.com>
2020-05-10 22:21:31 -0300

    Refatorando código de testes - parte 2

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,78 +1,90 @@
 require 'rails_helper'
 include ActionController::RespondWith
 
-
-
 RSpec.describe "Users", type: :request do
   describe "Rotas: Users" do
     let!(:usuario) do
       @userA = create(:user)
-      @userB = create(:user, first_name: 'Fernando', last_name: 'Moraes', email: 'fernando.moraes@ruptiva.com')
-      @userC = create(:user, first_name: 'Cátia', last_name: 'Abreu', email: 'catia.abreu@ruptiva.com')
+      @userB = create(:user, first_name: 'Fernando', last_name: 'Moraes', email: 'fernando.moraes@ruptiva.com', role: :user)
+      @userC = create(:user, first_name: 'Cátia', last_name: 'Abreu', email: 'catia.abreu@ruptiva.com', role: :user)
     end
 
-    describe "POST /auth" do
-      describe "status 200" do
-        it "cria um novo usuário" do
-          post "/auth", params:  {
-                                      "first_name": "João",
-                                      "last_name": "da Silva",
-                                      "email": "joao@email.com",
-                                      "password": "12345678",
-                                      "password_confirmation": "12345678"
-                                  }
-          expect(response).to have_http_status(:success)
-          expect(JSON.parse(response.body)['data']['email']).to eql("joao@email.com")
+    context "quando o usuário é do tipo 'ADMIN'" do
+      describe "POST /auth" do
+        describe "status 200" do
+          it "cria um novo usuário" do
+            post "/auth", params:  {
+                "first_name": "João",
+                "last_name": "da Silva",
+                "email": "joao@email.com",
+                "password": "12345678",
+                "password_confirmation": "12345678"
+            }
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body)['data']['email']).to eql("joao@email.com")
+          end
+        end
+      end
+
+      describe "GET /users" do
+        describe "status 200" do
+
+          it "retorna todos os usuários cadstrados" do
+            auth_params = faz_login_de_usuario(@userA)
+            get '/users', headers: auth_params
+
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body).size()).to eq(3)
+          end
+        end
+      end
+
+      describe "GET /users/1" do
+        describe "status 200" do
+          it "retorna os dados do usuário Maikel" do
+            auth_params = faz_login_de_usuario(@userA)
+
+            get '/users/1', headers: auth_params
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body)['first_name']).to eql("Maikel")
+            expect(JSON.parse(response.body)['last_name']).to eql("Bald")
+            expect(JSON.parse(response.body)['email']).to eql("maikel@ruptiva.com")
+          end
+        end
+      end
+
+      describe 'GET /users/3' do
+        describe "status 200" do
+          it "retorna os dados da usuária Cátia" do
+            auth_params = faz_login_de_usuario(@userA)
+
+            get '/users/3', headers: auth_params
+            expect(response).to have_http_status(:success)
+            expect(JSON.parse(response.body)['first_name']).to eql("Cátia")
+            expect(JSON.parse(response.body)['last_name']).to eql("Abreu")
+            expect(JSON.parse(response.body)['email']).to eql("catia.abreu@ruptiva.com")
+          end
         end
       end
     end
 
-    describe "GET /users" do
-      describe "status 200" do
+    context "quando o usuário é do tipo normal('user')" do
+      describe 'GET /users' do
+        describe "status 200" do
+          it 'impede de o usuário acessar esse tipo de informação pois não é admin' do
+            auth_params = faz_login_de_usuario(@userC)
 
-        it "retorna todos os usuários cadstrados" do
-          auth_params = faz_login_de_usuario(@userA)
-          get '/users', headers: auth_params
-
-          expect(response).to have_http_status(:success)
-          expect(JSON.parse(response.body).size()).to eq(3)
+            get '/users', headers: auth_params
+            expect(response).not_to permit(@userC)
+          end
         end
       end
+
     end
-
-    describe "GET /users/1" do
-      describe "status 200" do
-        it "retorna os dados do usuário Maikel" do
-          auth_params = faz_login_de_usuario(@userA)
-
-          get '/users/1', headers: auth_params
-          expect(response).to have_http_status(:success)
-          expect(JSON.parse(response.body)['first_name']).to eql("Maikel")
-          expect(JSON.parse(response.body)['last_name']).to eql("Bald")
-          expect(JSON.parse(response.body)['email']).to eql("maikel@ruptiva.com")
-        end
-      end
-    end
-
-    describe 'GET /users/3' do
-      describe "status 200" do
-        it "retorna os dados da usuária Cátia" do
-          auth_params = faz_login_de_usuario(@userA)
-
-          get '/users/3', headers: auth_params
-          expect(response).to have_http_status(:success)
-          expect(JSON.parse(response.body)['first_name']).to eql("Cátia")
-          expect(JSON.parse(response.body)['last_name']).to eql("Abreu")
-          expect(JSON.parse(response.body)['email']).to eql("catia.abreu@ruptiva.com")
-        end
-      end
-    end
-
     describe 'DELETE /auth' do
       describe "status 200" do
         it "faz soft-delete do usuário após o mesmo ter logado" do
          auth_params = faz_login_de_usuario(@userB)
-
           delete '/auth/', headers: auth_params
           expect(response).to have_http_status(:success)
         end
