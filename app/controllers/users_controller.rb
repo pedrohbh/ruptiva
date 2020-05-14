7d589d6353fbb1335354378201ec803fd6c8386c
Pedro Hoppe <pedrohbh@github.com>
2020-05-13 22:02:38 -0300

    Início da escrita da documentação

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,54 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   after_action :verify_authorized
 
+=begin
+@api {get} /users Lista Usuários Cadastrados
+@apiGroup Usuários
+@apiHeader {String} access-token Token de usuário
+@apiHeader {String} token-type Tipo de token de usuário
+@apiHeaderExample {json} Header
+  {"Authorization": "JWT xyz.abc.123.hgf"}
+@apiSuccess {Object[]} users Lista de usuários registrados
+@apiSuccess {Number} users.id Id de registro
+@apiSuccess {String} users.provider Tipo de autenticação
+@apiSuccess {String} users.uid Identificador único
+@apiSuccess {Boolean} users.allow_password_change Permite mudar senha?
+@apiSuccess {String} users.first_name Nome
+@apiSuccess {String} users.last_name Sobrenome
+@apiSuccess {Date} users.created_at Data de cadastro
+@apiSuccess {Date} users.updated_at Última data de atualização
+@apiSuccess {Date} users.deleted_at Data de exclusão
+@apiSuccess {String} users.role Id do usuário
+@apiSuccessExample {json} Sucesso
+HTTP/1.1 200 OK
+  [
+    {
+        "id": 1,
+        "provider": "email",
+        "uid": "Maikel",
+        "allow_password_change": false,
+        "first_name": "Maikel",
+        "last_name": "Bald",
+        "email": "maikel@ruptiva.com",
+        "created_at": "2020-05-12T14:19:49.192Z",
+        "updated_at": "2020-05-12T14:21:39.531Z",
+        "deleted_at": null,
+        "role": "admin"
+    },
+    {
+        "id": 2,
+        "provider": "email",
+        "uid": "joao8@email.com",
+        "allow_password_change": false,
+        "first_name": "João",
+        "last_name": "da Silva",
+        "email": "joao@email.com",
+        "created_at": "2020-05-12T14:25:56.653Z",
+        "updated_at": "2020-05-12T14:25:56.960Z",
+        "deleted_at": null,
+        "role": "user"
+    }]
+=end
   def index
     @user = User.all
     authorize User
